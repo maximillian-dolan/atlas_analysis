3a12c7b36ac5e6b3aafa9f9c61d55cd5cacd3801
maxdolan <maxdolan@eduroam-54-177.nomadic.bris.ac.uk>
2024-04-22 17:00:45 +0100

    bash script that just runs the analysis non-containerised

---
+++ hzz/hzz_bash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Remove the current saved plot
+rm data/graph.png
+
+# Record the start time
+start_time=$(date +%s)
+
+# Function to run the worker script
+run_worker() {
+    rank=$1
+    python worker/hzz_script.py --rank $rank
+}
+
+# Get input argument
+n=$1
+
+# Run the counter script
+python counter/hzz_counter.py --number_workers $n
+
+# Run worker scripts with ranks 0 to n-1
+for ((rank=0; rank<n; rank++)); do
+    run_worker $rank
+done
+
+# Run the collector script
+python collector/hzz_collector.py
+
+# Record end time when plot is actually produced
+while [ ! -f data/graph.png ]; do
+    sleep 1
+done
+end_time=$(date +%s)
+
+duration=$((end_time - start_time))
+echo "Total time taken for analysis: $duration seconds."
